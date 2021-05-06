eb3a47e1e63ab05a91d49e160feaa619872e2b46
Edson Batista <edson.batista@catho.com>
2021-05-05 22:35:58 -0300

    feat: configure next-auth with github oauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,28 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
-
 
 import style from './styles.module.scss';
 
 export function SingInButton() {
-  const isUserLoggedIn = true;
+  const [ session ] = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={style.signInButton}>
+  return session ? (
+    <button 
+      type="button"
+      className={style.signInButton}
+      onClick={ () => signOut()}
+    >
       <FaGithub color="#04d361"/>
       Edson Batista
       <FiX color="#737380"/>
     </button>
   ) : (
-    <button type="button" className={style.signInButton}>
+    <button
+      type="button"
+      className={style.signInButton}
+      onClick={ () => signIn()}
+    >
       <FaGithub color="#eba417"/>
       Sign in with Github
     </button>
